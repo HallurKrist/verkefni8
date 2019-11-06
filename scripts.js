175b0b6c2e8b06bcf6cd7a2174ade3e9c9561169
Hallur Kristinn Hallsson <hkh32@hi.is>
2019-11-06 15:57:43 +0000

    búið!!!

--- scripts.js
+++ scripts.js
@@ -13,40 +13,92 @@ const text = (() => {
   function init(_form, _items) {
     items = _items;
     _form.addEventListener('submit', formHandler);
-
-    // TODO láta hluti í _items virka
+    updateListeners();
   }
 
   function formHandler(e) {
     e.preventDefault();
-
-    console.log('halló heimur');
+    const texti = document.getElementsByClassName('form__input');
+    if(texti[0].value != ""){
+      add(texti[0].value); 
+    }
+    document.querySelector('.form').reset();
+    updateListeners();
   }
 
-  // event handler fyrir það að klára færslu
   function finish(e) {
+    const boxid = e.srcElement;
+    const stakid = boxid.parentElement;
+    stakid.classList.toggle('item--done')
   }
 
-  // event handler fyrir það að breyta færslu
   function edit(e) {
+    const texti = e.srcElement;
+    const inputid = el('input','item__edit');
+    inputid.setAttribute('type', 'text');
+    texti.parentNode.replaceChild(inputid,texti);
+    updateListeners();
   }
 
-  // event handler fyrir það að klára að breyta færslu
   function commit(e) {
+    if(e.keyCode === 13){
+      const inputid = e.srcElement;
+      const nyrTexti = inputid.value;
+      const span = el('span','item__text');
+      inputid.parentNode.replaceChild(span,inputid);
+      span.appendChild(document.createTextNode(nyrTexti));
+    }
   }
 
-  // fall sem sér um að bæta við nýju item
   function add(value) {
+    const listi = document.querySelector('ul');
+    const hlutur = el('li','item');
+    const input = el('input', 'item__checkbox');
+    const span = el('span','item__text');
+    const btn = el('button','item__button');
+    btn.appendChild(document.createTextNode('Eyða'));
+    span.appendChild(document.createTextNode(value));
+    hlutur.appendChild(input);
+    hlutur.appendChild(span);
+    hlutur.appendChild(btn);
+    listi.appendChild(hlutur)
   }
 
-  // event handler til að eyða færslu
   function deleteItem(e) {
+    const btn = e.srcElement;
+    const parent = btn.parentNode;
+    parent.parentNode.removeChild(parent);
   }
 
-  // hjálparfall til að útbúa element
-  function el(type, className, clickHandler) {
+  function el(type, className) {
+    const element = document.createElement(type);
+    element.setAttribute('class', className);
+    if(type === 'input')
+    {
+      element.setAttribute('type', 'checkbox');
+    }
+    return element;
   }
 
+  function updateListeners() {
+    let inputs = items.getElementsByClassName('item__checkbox');
+    for(cb of inputs){
+      cb.addEventListener('click', finish);
+    }
+    let edits = items.querySelectorAll('span');
+    for(verk of edits){
+      verk.addEventListener('click', edit);
+    }
+    let edits2 = items.getElementsByClassName('item__edit');
+    for(verk of edits2){
+      verk.addEventListener('keyup', commit);
+    }
+    let del = items.getElementsByClassName('item__button');
+    for(btn of del){
+      btn.addEventListener('click',deleteItem);
+    }
+  }
+ 
   return {
     init: init
   }
